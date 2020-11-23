b9cbe0083915426de4e92eeb6da445df76db1033
Raquel Cadete <57494565+RLCadete05@users.noreply.github.com>
2020-11-23 00:46:43 -0300

    part2

--- brasilEscola.py
+++ brasilEscola.py
@@ -4,8 +4,8 @@ import requests
 from bs4 import BeautifulSoup
 
 # verificar se a página está disponível
-def get_pagina_existente(object):
-    content = requests.get(object.strip())
+def get_pagina_existente(url):
+    content = requests.get(url.strip())
     return False if content.status_code == 404 else content.content
 
 def temas_e_links(url):
@@ -15,12 +15,11 @@ def temas_e_links(url):
     for option in options[1:]:
         print(option['value'] + '->' + option.text)
 
-class extrair_descricao(object):
-    def get_descricao_tema(self, soup):
-        soup = BeautifulSoup(get_pagina_existente(url), 'lxml')
-        secao_texto = soup.find('div', attrs={'id': 'secao_texto'})
-        divs = secao_texto.find_all('div')
-        print(divs[11].text)
+def get_descricao_tema(url):
+    soup = BeautifulSoup(get_pagina_existente(url), 'lxml')
+    secao_texto = soup.find('div', attrs={'id': 'secao_texto'})
+    divs = secao_texto.find_all('div')
+    print(divs[11].text)
 
 '''
 def get_essays(url):
@@ -48,27 +47,19 @@ def extrair_titulo_e_tema(url):
     print('\nTITULO: ', title)
     essay = soup.find('div', attrs={'class': 'conteudo-materia'})
     for content in essay.find_all('p')[1:]:
-        print(content.text)
+        print('\nREDAÇÃO: ', content.text)
 
-class ExtrairNota():
-    def get_redacoes(url):
-        soup = BeautifulSoup(get_pagina_existente(url), 'lxml')
-        tabela = soup.find('table', attrs={'id': 'redacoes_corrigidas'})
-        coluna = tabela.find_all('td')[1]
-        print(soup.find(id='redacoes_corrigidas').text)
-
-
+def get_redacoes(url):
+    soup = BeautifulSoup(get_pagina_existente(url), 'lxml')
+    tabela = soup.find('table', attrs={'id': 'redacoes_corrigidas'})
+    coluna = tabela.find_all('td')[1]
+    print(soup.find(id='redacoes_corrigidas').text)
 
 
 URL_BASE = 'https://vestibular.brasilescola.uol.com.br/banco-de-redacoes'
 url_theme = 'https://vestibular.brasilescola.uol.com.br/banco-de-redacoes/tema-abuso-de-autoridade-no-brasil.htm'
 url_essay = 'https://vestibular.brasilescola.uol.com.br/banco-de-redacoes/16257'
 temas_e_links(URL_BASE)
-
-#get_descricao_tema(url_theme)
-#descricao = extrair_descricao(url_theme)
-
-# get_redacoes(url_theme)
-
-#extrair_titulo_e_tema(url_essay)
-ExtrairNota(url_essay)
+get_descricao_tema(url_theme)
+extrair_titulo_e_tema(url_essay)
+get_redacoes(url_essay)
--- uolRedacoes.py
+++ uolRedacoes.py
@@ -4,27 +4,28 @@ import requests
 from bs4 import BeautifulSoup
 
 # verificar se a página está disponível
-def get_content_page(url):
+def get_pagina_existente(url):
     content = requests.get(url.strip())
     return False if content.status_code == 404 else content.content
 
 #ainda n funciona
-def themes_and_links(url):
-    soup = BeautifulSoup(get_content_page(url), 'lxml')
-    # select = soup.find('select', attrs={'id': 'selectBoxBR'})
-    hrefs = soup.select('select[class=flex-wrap]> href')
-    for href in hrefs[1:]:
-        print(href['value'] + '->' + href.link)
+def temas_e_links(url):
+    soup = BeautifulSoup(get_pagina_existente(url), 'lxml')
+    #select = soup.find('select', attrs={'id': 'selectBoxBR'})
+    options = soup.select('select[class=thumbnails-wrapper]> option')
+    for option in options[1:]:
+        print("TEMAS: ", option['value'] + '->' + option.link)
 
-def get_description(url):
-    soup = BeautifulSoup(get_content_page(url), 'lxml')
+        
+def get_descricao_tema(url):
+    soup = BeautifulSoup(get_pagina_existente(url), 'lxml')
     description = soup.find('span', attrs={'class': 'text'})
     spans = description.find_all('span')#Erro com find_all
     print(spans[11].description)
 
 
-def extract_content_essay(url):
-    soup = BeautifulSoup(get_content_page(url), 'lxml')
+def extrair_titulo_e_tema(url):
+    soup = BeautifulSoup(get_pagina_existente(url), 'lxml')
     #print(soup.find('p', attrs={'class': 'text-composition'}))
     '''try:
         theme = soup.find_all('div', attrs={'class': 'container-composition'}).h2
@@ -44,10 +45,8 @@ def extract_content_essay(url):
     for content in essay.find_all('p')[0:]:
         print('\nREDAÇÃO: ', content.text)
 
-
-
-def get_essays(url):
-    soup = BeautifulSoup(get_content_page(url), 'lxml')
+def get_redacoes(url):
+    soup = BeautifulSoup(get_pagina_existente(url), 'lxml')
     print('\nNOTA FINAL: ', soup.find('article', attrs={'class': 'rt-body'}).text)
 
 
@@ -56,9 +55,9 @@ URL_BASE = 'https://educacao.uol.com.br/bancoderedacoes/'
 url_theme = 'https://educacao.uol.com.br/bancoderedacoes/propostas/qualificacao-e-o-futuro-do-emprego.htm'
 url_essay = 'https://educacao.uol.com.br/bancoderedacoes/redacoes/qualificacoes-para-o-mercado-de-trabalho.htm'
 
-themes_and_links(URL_BASE)
-#get_description(url_theme)
-# get_essays(url_theme)
+temas_e_links(URL_BASE)
+#get_descricao_tema(url_theme)
+#get_redacoes(url_theme)
 
-extract_content_essay(url_essay)
-get_essays(url_essay)
+#extrair_titulo_e_tema(url_essay)
+get_redacoes(url_essay)
